bafd9d0ac5e117530359be6344cdc660f13f681b
Higino Silva <higino@gmail.com>
2016-12-30 18:12:16 +0000

    Final version of buttons implementation

--- main.js
+++ main.js
@@ -1,12 +1,69 @@
 
+
 (function () { // Avoid functions or variable namess collision with otheer libraries. Assumes a Universe in the global context
+    var global = this;
+    global.MAX_PARTICLES = global.MAX_PARTICLES || 100; // Maximum number of particles to be draw
+    global.MS_DELAY_BETWEEN_PARTICLES = global.MS_DELAY_BETWEEN_PARTICLES || 200; // Miliseconds to wait before drawing a new particle
+    global.GRAVITY = global.GRAVITY || 0.2; // Self explanatory. Negative gravituis implies bubbles will go up
+
+// Check that a canvas exist otherwise create it
+var canvas = document.getElementById('canvas');
+if( !canvas ) {
+    canvas = document.createElement("canvas");
+    canvas.setAttribute("width", "500");
+    canvas.setAttribute("id", "canvas");
+    canvas.setAttribute("height", "300");
+    canvas.style.border = "1px solid black";
+    document.body.appendChild(canvas);
+}
+// Append input fileds after 
+var div = document.createElement("div");
+var fieldSet = document.createElement("fieldSet");
+var numBubblesElement = document.createElement("input");
+numBubblesElement.value = MAX_PARTICLES;
+var numBubblesButton = document.createElement("button");
+numBubblesButton.innerText = "Set bubble #";
+numBubblesButton.addEventListener("click", function () {
+    MAX_PARTICLES = Number(numBubblesElement.value) || 2;
+    universe.length = MAX_PARTICLES; // Universe reached its end :-D. It will reset itself
+    numBubblesElement.value = MAX_PARTICLES;
+});
+var delayBetweenParticlesElement = document.createElement("input");
+delayBetweenParticlesElement.value = MS_DELAY_BETWEEN_PARTICLES;
+var delayBetweenParticlesButton = document.createElement("button");
+delayBetweenParticlesButton.innerText = "Set delay between bubbles";
+delayBetweenParticlesButton.addEventListener("click", function () {
+    MS_DELAY_BETWEEN_PARTICLES = Number(delayBetweenParticlesElement.value) || 3000;
+    universe.length = MAX_PARTICLES; // Universe reached its end :-D. It will reset itself   
+    delayBetweenParticlesElement.value = MS_DELAY_BETWEEN_PARTICLES; 
+});
+var gravityElement = document.createElement("input");
+gravityElement.setAttribute("type" , "range");
+gravityElement.setAttribute("min", "-10");
+gravityElement.setAttribute("max", "10");
+gravityElement.value = GRAVITY*10;
+gravityElement.addEventListener("pointermove", function () {
+    GRAVITY = Number(gravityElement.value/10) || 0;
+    gravityLabel.innerText = "Gravity: " + GRAVITY;
+});
+var gravityLabel = document.createElement("label");
+gravityLabel.innerText = "Gravity: " + gravityElement.value/10;
+
+fieldSet.appendChild(delayBetweenParticlesButton);
+fieldSet.appendChild(delayBetweenParticlesElement);
+fieldSet.appendChild(numBubblesButton);
+fieldSet.appendChild(numBubblesElement);
+fieldSet.appendChild(gravityElement);
+fieldSet.appendChild(gravityLabel);
+div.appendChild(fieldSet);
+document.body.appendChild(div);
+
+function onDelayBetweenParticlesClick(ev) {
+}
 
 (function init() {
     // Start animation loop
     requestAnimationFrame(function() { // Closure to set context available to animationLoop
-        var canvas = document.getElementById('canvas');
-        if( !canvas )
-            return function (){}; // There is no canvas, so nothing to do
         var ctx = canvas.getContext('2d');
         if( !ctx )
             return function (){}; // There is no canvas, so nothing to do
@@ -21,6 +78,7 @@
                 // 3. Draw animated shapes
                 // 4. Restore the canvas state
             ctx.globalCompositeOperation = 'destination-over';
+            //ctx.globalCompositeOperation = 'lighter';
             ctx.clearRect(0,0,canvas.width, canvas.height);
             ctx.save();
             universe.forEach(function (item) {
